64d2672416cf4ea469ac6c6946b490201d1b4297
Andreas Zunker <157129079+andreaszunker@users.noreply.github.com>
2024-01-28 18:44:50 +0100

    Update enumerate_minimum_weight_codewords.c

--- enumerate_minimum_weight_codewords.c
+++ enumerate_minimum_weight_codewords.c
@@ -95,9 +95,9 @@ void reduced_row_echelon_form(size_t rows, size_t columns, uint8_t matrix[rows][
     while (current_row < rows && pivot_column < columns) {
         // Find the pivot element with a non-zero value in the current column
         size_t pivot_row = current_row;
-        while (pivot_row < rows && matrix[pivot_row][pivot_column] == 0)
+        while (pivot_row < rows && matrix[pivot_row][pivot_column] == 0) {
             ++pivot_row;
-
+        }
         if (pivot_row >= rows) {
             // No non-zero pivot element found in the current column
             ++pivot_column;
@@ -112,11 +112,13 @@ void reduced_row_echelon_form(size_t rows, size_t columns, uint8_t matrix[rows][
             }
         }
         // Eliminate all elements of in pivot current column except the pivot itself
-        for (size_t row = 0; row < rows; ++row)
-            if (row != current_row && matrix[row][pivot_column] != 0)
-                for (size_t column = pivot_column; column < columns; ++column)
+        for (size_t row = 0; row < rows; ++row) {
+            if (row != current_row && matrix[row][pivot_column] != 0) {
+                for (size_t column = pivot_column; column < columns; ++column) {
                     matrix[row][column] ^= matrix[current_row][column];
-        
+                }
+            }
+        }
         ++current_row;
         ++pivot_column;
     }
@@ -149,9 +151,11 @@ unsigned long enumerate_subtree(
     size_t coset_index, size_t start_level, size_t stop_level, size_t message_size, int64_t start_message[], 
     uint8_t rate_profile[], uint8_t sibling_levels[], int64_t pretransform[][message_size]
     ) {
-    unsigned long A_wmin = 0UL;
     // Copy the message so the original message can be used later to enumerate the other sub-tree
-    int64_t *message = malloc(sizeof(int64_t[message_size])); memcpy(message, start_message, sizeof(int64_t[message_size]));
+    int64_t *message = malloc(sizeof(int64_t[message_size])); 
+    memcpy(message, start_message, sizeof(int64_t[message_size]));
+    
+    unsigned long A_wmin = 0UL;
     update_message(coset_index, start_level, message);
     for (size_t level = start_level + 1; level <= stop_level; ++level) {
         if (rate_profile[level]) { 
@@ -163,8 +167,9 @@ unsigned long enumerate_subtree(
         } else {
             // Einzelchild level of the intersection tree
             int64_t bit = 0L; 
-            for (size_t index = coset_index >> 6; index < ((level-1) >> 6)+1; ++index)
+            for (size_t index = coset_index >> 6; index < ((level-1) >> 6)+1; ++index) {
                 bit ^= message[index] & pretransform[level][index];
+            }
             if (__builtin_popcountl(bit) % 2 != ((message[level >> 6] >> (level & 63)) & 1)) {
                 // The pre-transformation does not match with the current message
                 if (sibling_levels[level]) {
@@ -217,19 +222,20 @@ struct enumeration_result enumerate_minimum_weight_codewords(size_t K, size_t N,
     fast_transform2(K, N, pretransform); // get the pre-transformation matrix
     reduced_row_echelon_form(K, N, pretransform); // bring the pre-transformation matrix into RREF 
     
-    // Expand the pre-transform into a square matrix and store it with bitwise columns and in Fortran order
+    // Expand the pre-transform into a N×N matrix and store it with bitwise columns and in Fortran order
     uint8_t *rate_profile = calloc(N, sizeof(uint8_t)); // indicator of the sibling level of the PTPC
-    size_t message_size = ((N-1) >> 6)+1; // the message is stored bitwise
+    size_t message_size = ((N-1) >> 6)+1; // the message is stored bitwise -> faster checking of the dynamic frozen bits
     int64_t (*expanded_pretransform)[message_size] = calloc(N*message_size, sizeof(int64_t)); 
                 
     size_t pivot_column = 0;
     for (size_t row = 0; row < K; ++row) {
-        // find the pivot point columns of the pre-transformation matrix -> information bits
-        while (pretransform[row][pivot_column] == 0)
+        // Find the pivot point columns of the pre-transformation matrix -> information bits
+        while (pretransform[row][pivot_column] == 0) {
             ++pivot_column;
-        for (size_t column = 0; column < N; ++column)
-            // store the rows in an expanded N×N pre-transformation with bitwise columns -> faster checking of the dynamic frozen bits
+        }
+        for (size_t column = 0; column < N; ++column) {
             expanded_pretransform[column][pivot_column >> 6] |= (int64_t)pretransform[row][column] << (pivot_column & 63);
+        }
         rate_profile[pivot_column] = 1;  
     } 
     int64_t *message = malloc(sizeof(int64_t[message_size])); 
@@ -237,9 +243,11 @@ struct enumeration_result enumerate_minimum_weight_codewords(size_t K, size_t N,
     struct enumeration_result result = {UINT_MAX, 0UL};
     
     // Find minimum Hamming weight "wmin" of a coset leader
-    for (size_t index = 0; index < N; ++index)
-        if (rate_profile[index])
+    for (size_t index = 0; index < N; ++index) {
+        if (rate_profile[index]) {
             result.wmin = min(result.wmin, 1U << __builtin_popcount(index));
+        }
+    }
     
     // Count the "wmin"-weight codewords in each coset
     for (size_t coset_index = 0; coset_index < N; ++coset_index) {
@@ -249,13 +257,17 @@ struct enumeration_result enumerate_minimum_weight_codewords(size_t K, size_t N,
         memset(message, 0, sizeof(int64_t[message_size])); memset(sibling_levels, 0, sizeof(uint8_t[N])); 
         
         // Find the level after which the pre-transformation cannot prevent the formation of "wmin"-weight codewords and compute their number
-        size_t stop_level = coset_index; int coefficient = 1;
+        size_t stop_level = coset_index; 
+        unsigned long coefficient = 1;
         for (size_t level = coset_index+1; level < N; ++level) {
             if (__builtin_popcount(~coset_index & level) == 1) {
                 sibling_levels[level] = 1;
-                if (rate_profile[level]) coefficient *= 2;
+                if (rate_profile[level]) {
+                    coefficient *= 2;
+                }
             } else if (rate_profile[level] == 0) {
-                stop_level = level; coefficient = 1;
+                stop_level = level; 
+                coefficient = 1;
             }
         }
         result.A_wmin += coefficient * enumerate_subtree(
@@ -284,8 +296,11 @@ int main() {
     reed_muller_rate_profile(r, m, rate_profile);
     
     size_t K = 0;
-    for (size_t index = 0; index < N; ++index) 
-        if (rate_profile[index]) ++K;
+    for (size_t index = 0; index < N; ++index) {
+        if (rate_profile[index]) {
+            ++K;
+        }
+    }
     
     // Construct the generator matrix
     uint8_t (*generator_matrix)[N] = malloc(sizeof(uint8_t[K][N]));
@@ -297,10 +312,12 @@ int main() {
     
     int runs = 1000;
     clock_t start = clock();
-    for (int i = 0; i < runs; ++i)
+    for (int i = 0; i < runs; ++i) {
         result = enumerate_minimum_weight_codewords(K, N, generator_matrix);
+    }
     clock_t end = clock();
     
+    // Print result
     printf("PAC RM(%d,%d):\n", r, m);
     printf("wmin: %u, A_wmin: %lu\n", result.wmin, result.A_wmin);
     printf("Average elapsed time of %d runs: %.3e s\n", runs, (double)(end - start) / (CLOCKS_PER_SEC * runs));
